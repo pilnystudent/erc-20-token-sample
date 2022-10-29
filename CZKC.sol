ebcba99fb2055a1e148c58812c93f502a5fc4e43
Remix Workspace <>
2022-10-29 16:42:48 +0200

    commit

--- CZKC.sol
+++ CZKC.sol
@@ -3,5 +3,7 @@
 pragma solidity ^0.8.17;
 
 contract CZKC {
-    
+    string public constant name = "CZK Coin";
+    string public constant symbol = "CZKC";
+    uint8 public constant decimals = 18;
 }
