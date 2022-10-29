6b4ff75a67135ff63969baed9f9543524e1328f2
Remix Workspace <>
2022-10-29 17:24:42 +0200

    commit

--- contracts/CZKC.sol
+++ contracts/CZKC.sol
@@ -1,12 +1,66 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.17;
 
-contract CZKC {
+import "./IERC20.sol";
 
-    string public constant name = "CZK Coin";
-    string public constant symbol = "CZKC";
-    uint8 public constant decimals = 18;
+contract CZKC is IERC20{
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+    uint256 private _totalSupply;
 
-    
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    constructor() {
+        _name = "CZK Coin";
+        _symbol = "CZKC";
+        _decimals = 18;
+    }
+
+    function name() external view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() external view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() external view returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) external view returns (uint256){
+        return _allowances[owner][spender];
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        _balances[msg.sender] -= amount;
+        _balances[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool){
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address spender, address recipient, uint256 amount) external returns (bool){
+        _allowances[spender][msg.sender] -= amount;
+        _balances[spender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(spender, recipient, amount);
+        return true;
+    }
 }
